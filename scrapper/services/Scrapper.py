7a955366433f0f44ed73d047f0e88b3313509990
Salman Mapkar <salmansmapkar@gmail.com>
2022-06-22 13:18:17 +0530

    Making sure selenium scripts runs on heroku

--- scrapper/services/Scrapper.py
+++ scrapper/services/Scrapper.py
@@ -3,14 +3,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from ..models import AirReport
+import os
 
 
 def ScrapeData():
+
     #Download Drivers
+    """
     options = webdriver.ChromeOptions()
     options.headless = True
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-gpu");
     driver = webdriver.Chrome(ChromeDriverManager().install() ,options=options)
-
+    """
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
     #Scrape Data
